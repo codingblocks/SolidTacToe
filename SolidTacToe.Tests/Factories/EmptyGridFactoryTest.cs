8662c96c85fc874b70639f8f83a601b7afbc07e7
joe <joe@fabric.com>
2014-03-27 11:48:08 -0400

    Adding tests for factories

---
+++ SolidTacToe.Tests/Factories/EmptyGridFactoryTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolidTacToe.Definitions;
+using SolidTacToe.Factories;
+
+namespace SolidTacToe.Tests.Factories
+{
+    [TestClass]
+    public class EmptyGridFactoryTest
+    {
+        [TestMethod]
+        public void AllSlotsCreatedEmpty()
+        {
+            var target = new EmptyGridFactory();
+            var result = target.Create<GridFake>(2);
+            Assert.AreEqual(Token.Empty, result.Get(0, 0));
+            Assert.AreEqual(Token.Empty, result.Get(0, 1));
+            Assert.AreEqual(Token.Empty, result.Get(1, 0));
+            Assert.AreEqual(Token.Empty, result.Get(1, 1));
+        }
+    }
+
+    internal class GridFake : IGrid, IGridMatrix
+    {
+        public Token[,] Slots { set; private get; }
+        public int Size { get; set; }
+        public Token Get(int x, int y)
+        {
+            return Slots[x, y];
+        }
+
+        public void Set(Token t, int x, int y)
+        {
+            Slots[x, y] = t;
+        }
+    }
+
+}
---
+++ SolidTacToe.Tests/Factories/StandardGameConditionsFactoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SolidTacToe.Definitions;
+using SolidTacToe.Factories;
+using SolidTacToe.GameOverConditions;
+
+namespace SolidTacToe.Tests.Factories
+{
+    [TestClass]
+    public class StandardGameConditionsFactoryTest
+    {
+        private StandardGameConditionsFactory Target { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var grid = new Mock<IGrid>().Object;
+            Target = new StandardGameConditionsFactory { Grid = grid };
+        }
+
+        [TestMethod]
+        public void SameNumberOfWinConditionsExistForAllTokens()
+        {
+            var result = Target.Create();
+            var xCount = result.Count(x => x is IGameWonCondition && ((IGameWonCondition)x).Winner == Token.X);
+            var yCount = result.Count(x => x is IGameWonCondition && ((IGameWonCondition)x).Winner == Token.O);
+
+            Assert.AreEqual(xCount, yCount);
+        }
+
+        [TestMethod]
+        public void NoMovesLeftConditionIsLast()
+        {
+            var result = Target.Create();
+            Assert.IsTrue(result.Last() is NoMovesLeftCondition);
+        }
+    }
+}
